ae891a32a00f24affd2e9f1014ca6da36d2afb4a
example <example@example.com>
2021-12-07 16:55:32 -0800

    Fix code formating

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
     bg.addImage(bgImg)
     bg.scale = 1.1
 
-//player
+    //player
     player = createSprite(210,10, 25, 25);
     player.scale = 1 
  
@@ -25,26 +25,24 @@ function setup() {
     //ground
     ground = createSprite(displayWidth/2, displayHeight - 110, displayWidth, 30)  
     
-    
-     //platform1 
-     platform1 = createSprite(200, 580, 100, 25)    
+    //platform1 
+    platform1 = createSprite(200, 580, 100, 25)    
 
-   //platform2
-   platform2 = createSprite(400, 510, 60, 25)   
+    //platform2
+    platform2 = createSprite(400, 510, 60, 25)   
    
-   //platform3
+    //platform3
     platform3 = createSprite(600, 440, 120, 25)
 
-     //platform4
-     platform4 = createSprite(800, 370, 120, 25)
+    //platform4
+    platform4 = createSprite(800, 370, 120, 25)
 
-     //platform5
-     platform5 = createSprite(1000, 300, 120, 25)
+    //platform5
+    platform5 = createSprite(1000, 300, 120, 25)
 
-     //platform6
-     platform6 = createSprite(1200, 230, 120, 25)
+    //platform6
+    platform6 = createSprite(1200, 230, 120, 25)
 }
-
 
 function draw() {
     background(0); 
@@ -71,10 +69,6 @@ function draw() {
     if (keyDown("a")) {
         player.x -= 3
     }
-
-    
-
-    
 
 drawSprites();
 
